423ce7eae4424278e1504d4f3f3ebb50e13eda7d
andrii0574 <0574@ukr.net>
2017-01-26 02:51:44 +0200

    First Commit

---
+++ HomeWork1/FifthClass.java
@@ -0,0 +1,10 @@
+package HomeWork1;
+
+/**
+ * Created by Z570 on 26.01.2017.
+ */
+public class FifthClass {
+    public static void main(String[] args) {
+        System.out.println("Мой пятый класс на Java");
+    }
+}
---
+++ HomeWork1/FourthClass.java
@@ -0,0 +1,10 @@
+package HomeWork1;
+
+/**
+ * Created by Z570 on 26.01.2017.
+ */
+public class FourthClass {
+    public static void main(String[] args) {
+        System.out.println("Мой четвертый класс на Java");
+    }
+}
