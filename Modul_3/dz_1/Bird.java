95492a00483432aade0029c4b1d3b2c02e33b51f
andrii0574 <0574@ukr.net>
2017-02-21 10:06:25 +0200

    Second Commit Module 3 DZ 1

--- Modul_3/dz_1/Bird.java
+++ Modul_3/dz_1/Bird.java
@@ -4,15 +4,31 @@ package Modul_3.dz_1;
  * Created by Z570 on 09.02.2017.
  */
 public class Bird {
-    public static void main(String[] args) {
-        Bird birdSing = new Bird();
-        birdSing.sing("I am walking");
-        birdSing.sing("I am flying");
-        birdSing.sing("I am singing");
-        birdSing.sing("I am Bird");
+    private static final String  bird = "I am walking \nI am flying \nI am singing \nI am Bird \n";
+    private String name;
+
+    public Bird() {
     }
 
-    private void sing(String song){
-        System.out.println(song);
+    public Bird(String name) {
+        this.name = name;
     }
+
+    public static void main(String[] args) {
+        Bird birdSing = new Bird();
+        birdSing.sing();
+
+        Bird birdVoody = new Bird("Voody");
+        birdVoody.sing();
+
+
+    }
+
+
+    private void sing(){
+        if(name != null){ System.out.println("I am " + name);}
+        System.out.println(bird);
+    }
+
+
 }
